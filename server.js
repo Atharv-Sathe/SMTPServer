5237e002e8a68b724d6cfeca0820e672cbde4ba2
AthSat <atharvsathe28704@gmail.com>
2025-04-21 23:19:27 +0530

    Setup basic SMTP-Server

---
+++ server.js
@@ -0,0 +1,29 @@
+import { SMTPServer } from "smtp-server";
+
+const server = new SMTPServer({
+  allowInsecureAuth: true,
+  authOptional: true,
+  onConnect(session, cb) {
+    console.log("onConnect: ", session.id);
+    cb();
+  },
+  onMailFrom(address, session, cb) {
+    console.log("Mail From: ", address.address, session.id);
+    cb();
+  },
+  onRcptTo(address, session, cb) {
+    console.log("On Recpt To: ", address.address, session.id);
+    cb();
+  },
+  onData(stream, session, cb) {
+    stream.pipe(process.stdout);
+    stream.on("data", (data) => {
+      console.log("onData: ", data.toString());
+    });
+    stream.on("end", cb);
+  },
+});
+
+server.listen(25, "Vision", () => {
+  console.log("SMTP Server Listening on 25!");
+});
